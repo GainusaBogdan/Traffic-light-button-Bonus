7e2df9a22258432b2f14714b67fbff815d5ff4b0
GainusaBogdan <130216860+GainusaBogdan@users.noreply.github.com>
2023-05-05 16:12:17 +0300

    Update script.js
    "var -> let
    sa scapam si de codul repetitiv"

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-colorID = 0;
+let colorID = "red";
 
-var buttonLightChanger = document.getElementById("btnTrafficLight");
+let buttonLightChanger = document.getElementById("btnTrafficLight");
 
 intervalID = setInterval(colorButton, 10000);
 
 function colorButton() {
-  if (colorID == 0) {
-    buttonLightChanger.style.background = "yellow";
-    buttonLightChanger.style.border = "yellow";
-    ++colorID;
+  switch(colorID) {
+    case "red":
+      colorID = "yellow";
+      break;
+    case "yellow":
+      colorID = "green";
+      break;
+    case "green":
+      colorID = "red";
+      break;
   }
-  else if (colorID == 1) {
-    buttonLightChanger.style.background = "green";
-    buttonLightChanger.style.border = "green";
-    ++colorID;
-  }
-  else if (colorID == 2) {
-    buttonLightChanger.style.background = "red";
-    buttonLightChanger.style.border = "red";
-    colorID = 0;
-  }
+    buttonLightChanger.style.background = colorID;
+    buttonLightChanger.style.border = colorID;
   clearInterval(intervalID);
   intervalID = setInterval(colorButton, 10000);
 }
