be45c2dcbc2c0638f5415eeb8e6247dfb6b5f3ee
GainusaBogdan <130216860+GainusaBogdan@users.noreply.github.com>
2023-05-10 17:24:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let colorID = 0;
 const colors = ["red" , "yellow" , "green"];
 let buttonLightChanger = document.getElementById("btnTrafficLight");
-let intervalID = setInterval(colorButton, 10000);
+setInterval(colorButton, 10000);
 
 buttonLightChanger.onclick = colorButton;
 
 function colorButton() {
-  colorID = colorID >= colors.length -1 ? 0 : ++colorID;
-  buttonLightChanger.style.background = colors[colorID];
-  buttonLightChanger.style.border = colors[colorID];
-  clearInterval(intervalID);
-  intervalID = setInterval(colorButton, 10000);
+    colorID = colorID >= colors.length -1 ? 0 : ++colorID;
+    buttonLightChanger.style.background = colors[colorID];
+    buttonLightChanger.style.border = colors[colorID];
 }
